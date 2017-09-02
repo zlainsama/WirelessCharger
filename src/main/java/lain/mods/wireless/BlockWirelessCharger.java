6846e7494f1f5bbf5c34c7c795f9e03acdd76df5
Lain MultipleInstances <zlainsama@163.com>
2017-09-02 21:15:17 +0800

    logic

--- src/main/java/lain/mods/wireless/BlockWirelessCharger.java
+++ src/main/java/lain/mods/wireless/BlockWirelessCharger.java
@@ -1,11 +1,18 @@
 package lain.mods.wireless;
 
+import lain.mods.wireless.TileEntityWirelessCharger.EnergyStorage;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -29,6 +36,21 @@ public class BlockWirelessCharger extends Block implements ITileEntityProvider
     public TileEntity createNewTileEntity(World arg0, int arg1)
     {
         return new TileEntityWirelessCharger();
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (!worldIn.isRemote)
+        {
+            TileEntity tile = worldIn.getTileEntity(pos);
+            if (tile instanceof TileEntityWirelessCharger)
+            {
+                EnergyStorage energy = ((TileEntityWirelessCharger) tile).getEnergyStorage();
+                playerIn.sendStatusMessage(new TextComponentString(String.format("%d/%d", energy.getEnergyStored(), energy.getMaxEnergyStored())), true);
+            }
+        }
+        return true;
     }
 
 }
--- src/main/java/lain/mods/wireless/ConfigOptions.java
+++ src/main/java/lain/mods/wireless/ConfigOptions.java
@@ -10,8 +10,8 @@ public class ConfigOptions
     {
         Configuration config = new Configuration(file);
 
-        Range = config.getInt("Range", Configuration.CATEGORY_GENERAL, 40, 1, 4000, "radius");
-        TransferRate = config.getInt("TransferRate", Configuration.CATEGORY_GENERAL, 30000, 1, 1000000000, "per item");
+        Range = config.getInt("Range", Configuration.CATEGORY_GENERAL, 40, 1, 10000, "Players in this radius will get charged");
+        TransferRate = config.getInt("TransferRate", Configuration.CATEGORY_GENERAL, 30000, 1, 1000000000, "Energy transfer rate per item");
 
         if (config.hasChanged())
             config.save();
